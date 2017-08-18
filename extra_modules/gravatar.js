3b78ac4fa564f5220f987ddead1ac1631f9f0774
Labib666 <ewmahadicd@gmail.com>
2017-08-18 07:30:26 +0600

    added better placeholders

--- extra_modules/gravatar.js
+++ extra_modules/gravatar.js
@@ -3,7 +3,7 @@ var md5 = require('md5');
 
 // making gravatar url with given dimension
 var gravatarURL = function(user,dimension) {
-	var defaultURL = encodeURIComponent("https://via.placeholder.com/"+dimension+"x"+dimension);
+	var defaultURL = encodeURIComponent("https://api.adorable.io/avatars/"+dimension+"/"+user.username);
 	return "https://www.gravatar.com/avatar/" + md5(user.email.toLowerCase())
 								+ "?s="+dimension+"&d=" + defaultURL;
 }
